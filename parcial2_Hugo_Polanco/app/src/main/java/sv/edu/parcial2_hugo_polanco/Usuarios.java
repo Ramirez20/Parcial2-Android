a7e3f4cfad774bdad861a9e1ad4c7cfa14d27f51
Ramirez20 <70790682+Ramirez20@users.noreply.github.com>
2023-03-22 21:42:26 -0600

    Creacion de variables

---
+++ parcial2_Hugo_Polanco/app/src/main/java/sv/edu/parcial2_hugo_polanco/Usuarios.java
@@ -0,0 +1,25 @@
+package sv.edu.parcial2_hugo_polanco;
+
+public class Usuarios {
+    private String nombre;
+    private String cargo;
+    private String empresa;
+
+    public Usuarios(String nombre, String cargo, String empresa) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.empresa = empresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+}
